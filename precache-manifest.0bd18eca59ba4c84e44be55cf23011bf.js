919da09fda7bd694a5fe24c029dc7fe53f45e5c5
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2022-10-27 02:39:19 +0200

    first commit

---
+++ precache-manifest.0bd18eca59ba4c84e44be55cf23011bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc35c3811c9252ef9f52",
+    "url": "/portfolj/static/css/main.93f01050.chunk.css"
+  },
+  {
+    "revision": "dc35c3811c9252ef9f52",
+    "url": "/portfolj/static/js/main.dc35c381.chunk.js"
+  },
+  {
+    "revision": "ba5a931919b9f7b3e918",
+    "url": "/portfolj/static/js/1.ba5a9319.chunk.js"
+  },
+  {
+    "revision": "67c50c5cb7c765dd1cc1",
+    "url": "/portfolj/static/js/2.67c50c5c.chunk.js"
+  },
+  {
+    "revision": "4c1e42aeb8cc3ebcb8e7",
+    "url": "/portfolj/static/js/runtime~main.4c1e42ae.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/portfolj/static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "abc93f15661e98cf83d1e3382bf246d3",
+    "url": "/portfolj/index.html"
+  }
+];
