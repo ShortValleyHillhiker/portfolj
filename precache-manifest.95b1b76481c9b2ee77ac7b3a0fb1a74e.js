e8680527a512323c8328a29e7668b91d51f11b06
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2022-10-27 18:09:50 +0200

    Updates

---
+++ precache-manifest.95b1b76481c9b2ee77ac7b3a0fb1a74e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a51ea6acf4026c40379",
+    "url": "/static/css/main.93f01050.chunk.css"
+  },
+  {
+    "revision": "2a51ea6acf4026c40379",
+    "url": "/static/js/main.2a51ea6a.chunk.js"
+  },
+  {
+    "revision": "ba5a931919b9f7b3e918",
+    "url": "/static/js/1.ba5a9319.chunk.js"
+  },
+  {
+    "revision": "67c50c5cb7c765dd1cc1",
+    "url": "/static/js/2.67c50c5c.chunk.js"
+  },
+  {
+    "revision": "f1f855bd2c4129ec593a",
+    "url": "/static/js/runtime~main.f1f855bd.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "f51e2311c34019c8e7e60712de1c42ec",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolj/precache-manifest.7247df805cb8b2aadb780335b785827e.js"
+  "/precache-manifest.95b1b76481c9b2ee77ac7b3a0fb1a74e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolj/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
