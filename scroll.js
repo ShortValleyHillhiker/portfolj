12f3f4d4043cb80b369f75c8562b9273bbd9fb5d
ShortValleyHillhiker <114674841+ShortValleyHillhiker@users.noreply.github.com>
2023-01-09 03:13:18 +0100

    BackToTop

---
+++ scroll.js
@@ -0,0 +1,14 @@
+function scrollOne() {
+  var element = document.getElementById("one");
+  element.scrollIntoView();
+}
+
+function scrollTwo() {
+  var element = document.getElementById("two");
+  element.scrollIntoView();
+}
+
+function scrollHome() {
+  var element = document.getElementById("home");
+  element.scrollIntoView();
+}
